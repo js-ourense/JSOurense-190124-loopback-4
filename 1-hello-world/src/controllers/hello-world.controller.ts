80eba96db78054a1a374430c88bacda0f8a292dc
Martín Molina Álvarez <tm07081995@gmail.com>
2019-01-21 20:55:05 +0100

    First commit

---
+++ 1-hello-world/src/controllers/hello-world.controller.ts
@@ -0,0 +1,29 @@
+import {
+  get,
+  RestBindings,
+  Request,
+  param,
+  ParameterObject,
+} from '@loopback/rest';
+import {inject} from '@loopback/core';
+
+// Uncomment these imports to begin using these cool features!
+
+// import {inject} from '@loopback/context';
+
+const helloNameSpec: ParameterObject = {
+  name: 'name',
+  in: 'query',
+  required: true,
+};
+
+export class HelloWorldController {
+  constructor(
+    @inject(RestBindings.Http.REQUEST) private readonly req: Request,
+  ) {}
+
+  @get('/hello')
+  hello(@param(helloNameSpec) name: string) {
+    return `Hello ${name}`;
+  }
+}
